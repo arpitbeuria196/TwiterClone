ce2a98dc2156e66e51cf594005c8178779e4b472
Arpit Beuria <arpitbeuria196@gmail.com>
2024-11-24 19:37:07 +0000

    basic Frontend Setup Done

---
+++ frontend/src/store/UserSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+
+    name: 'user',
+    initialState:null,
+
+    reducers:{
+        LoginUser :(state,action)=>
+        {
+            return action.payload;
+        },
+        SignUpUser : (state,action)=>
+        {
+            return action.payload;
+        },
+        LogOutUser : (state)=>
+        {
+            return null;
+        }
+    }
+})
+
+export const {LogOutUser,LoginUser,SignUpUser} = userSlice.actions;
+export default userSlice.reducer
